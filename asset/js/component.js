aa0a6d31be62df6d26798352b4884567f7497337
herdyy69 <herdyansah203@gmail.com>
2022-09-29 12:49:57 +0700

    29 09 2022

---
+++ asset/js/component.js
@@ -0,0 +1,18 @@
+function navbar(){
+    return (
+        "<nav class="homeNav" id="overlaynav">
+                <h1 class="myName">HERDYANSAH</h1>
+                <p class="stack">
+                  <small>FRONTEND WEB DEVELOPER</small>
+                </p>
+                <ul id="list">
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="config/profiles.html">Profil</a></li>
+                  <li><a href="config/educations.html">Education</a></li>
+                  <li><a href="config/experiences.html">Experience</a></li>
+                  <li><a href="config/projects.html">Project</a></li>
+                  <li><a href="config/skills.html">Skill</a></li>
+                </ul>
+              </nav>"
+    );
+}
